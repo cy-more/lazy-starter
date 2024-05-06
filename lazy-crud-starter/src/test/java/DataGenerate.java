43775db0d13e5d60fcf411cbac07352eaa07733a
6618732232917205 <qwaszx123>
2024-05-06 16:34:58 +0800

    feat:代码生成器

--- lazy-crud-starter/src/test/java/DataGenerate.java
+++ lazy-crud-starter/src/test/java/DataGenerate.java
@@ -2,6 +2,8 @@ import com.baomidou.mybatisplus.generator.FastAutoGenerator;
 import com.baomidou.mybatisplus.generator.config.OutputFile;
 import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
 import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+import com.lazy.crud.mybatis.YsServiceImpl;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.sql.Types;
 import java.util.Collections;
@@ -14,12 +16,11 @@ import java.util.Collections;
 public class DataGenerate {
 
     public static void main(String[] args) {
-        FastAutoGenerator.create("jdbc:mysql://localhost:3307/group_report?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8"
+        FastAutoGenerator.create("jdbc:mysql://10.92.100.253:3307/tjuacp_new?useUnicode=true&characterEncoding=utf8&zeroDateTimeBehavior=convertToNull&useSSL=true&serverTimezone=GMT%2B8"
                         , "root", "123456")
                 .globalConfig(builder -> {
                     builder.author("cy") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
-                            .fileOverride() // 覆盖已生成文件
                             .outputDir("D://IDEA//dataGenerate"); // 指定输出目录
                 })
                 .dataSourceConfig(builder -> builder.typeConvertHandler((globalConfig, typeRegistry, metaInfo) -> {
@@ -33,20 +34,34 @@ public class DataGenerate {
                 }))
                 .packageConfig(builder -> {
                     builder.parent("cn.tohealth") // 设置父包名
-                            .moduleName("groupReport") // 设置父包模块名
+                            .moduleName("tjuacp") // 设置父包模块名
                             .pathInfo(Collections.singletonMap(OutputFile.xml, "D://IDEA//dataGenerate")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("group_report_catalogue,group_report_content_abnormal,group_report_content_abnormal_his,group_report_content_abnormal_pe_user_info_ref,group_report_content_abnormal_pe_user_info_ref_his,group_report_content_catalogue,group_report_content_conclusion,group_report_content_conclusion_his,group_report_content_config_field,group_report_content_detail_field_his,group_report_content_other_instructions,group_report_content_pe_user_info,group_report_content_pe_user_info_his,group_report_content_project_conclusion,group_report_content_project_conclusion_his,group_report_content_reading_suggestions,group_report_content_suggestion_explain,group_report_content_suggestion_explain_his,group_report_params,group_report_task,group_report_task_his,group_report_task_ref,group_report_task_ref_his,group_report_template,group_report_template_catalogue_ref,group_report_template_catalogue_ref_his"); // 设置需要生成的表名
+                    builder.addInclude("system_tenant_misc,system_tenant_misc_user"); // 设置需要生成的表名
 //                            .addTablePrefix("t_", "c_"); // 设置过滤表前缀
 
                     builder.entityBuilder()
-                            .enableLombok()
                             .enableFileOverride()
                             .enableChainModel()
-//                            .superClass(BaseTenantBO.class)
-//                            .addIgnoreColumns("id","tenant_id","created_id","created_name","created_time","updated_id","updated_name","updated_time","deleted")
-                    ;
+
+                            .enableLombok()
+                            .enableChainModel();
+
+                    builder.controllerBuilder()
+                            .enableFileOverride()
+                            .enableRestStyle();
+
+                    builder.serviceBuilder()
+                            .enableFileOverride()
+                            .superServiceImplClass(YsServiceImpl.class);
+
+                    builder.mapperBuilder()
+                            .enableFileOverride()
+                            .mapperAnnotation(Mapper.class)
+                            .enableBaseResultMap()
+                            .enableBaseColumnList();
+
 
                 })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
